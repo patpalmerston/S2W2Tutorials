fa270448ae7d9a8c75cd2f2f1c03f3b3b87e7fd9
Pat Palmerston <patpalmerston@gmail.com>
2019-07-15 23:44:45 -0700

    number buttons mapped

--- lambda-calculator-master/src/App.js
+++ lambda-calculator-master/src/App.js
@@ -1,10 +1,11 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
 // STEP 4 - import the button and display components
+import Numbers from './components/ButtonComponents/NumberButtons/Numbers';
 // Don't forget to import any extra css/scss files you build into the correct component
 
 // Logo has already been provided for you. Do the same for the remaining components
-import Logo from "./components/DisplayComponents/Logo";
+import Logo from './components/DisplayComponents/Logo';
 
 function App() {
   // STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
@@ -14,10 +15,11 @@ function App() {
   // Don't forget to pass the functions (and any additional data needed) to the components as props
 
   return (
-    <div className="container">
+    <div className='container'>
       <Logo />
-      <div className="App">
+      <div className='App'>
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
+        <Numbers />
       </div>
     </div>
   );
--- lambda-calculator-master/src/components/ButtonComponents/NumberButtons/Numbers.js
+++ lambda-calculator-master/src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { useState } from 'react';
 
 //import any components needed
+import NumberButton from './NumberButton';
 
 //Import your array data to from the provided data file
+import numbers from '../../../data';
 
 const Numbers = () => {
   // STEP 2 - add the imported data to state
+  const [number, setNumber] = useState(numbers);
+  console.log({ number });
+
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+      {number.map(number => (
+        <NumberButton number={number} />
+      ))}
     </div>
   );
 };
+export default Numbers;
