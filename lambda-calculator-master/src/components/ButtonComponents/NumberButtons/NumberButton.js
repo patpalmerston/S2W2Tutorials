16d351115f4a43e3af704132bdc63667c8087335
Pat Palmerston <patpalmerston@gmail.com>
2019-07-16 10:25:19 -0700

    numbers buttons mapped and displayed

--- lambda-calculator-master/src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ lambda-calculator-master/src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,12 +1,12 @@
-import React from "react";
+import React from 'react';
 
-const NumberButton = () => {
-  return (
-    <>
-      {/* Display a button element rendering the data being passed down from the parent container on props */}
-      NumberButton Component
-    </>
-  );
+const NumberButton = ({ number }) => {
+	return (
+		<>
+			{/* Display a button element rendering the data being passed down from the parent container on props */}
+			<button>{number}</button>
+		</>
+	);
 };
 
 export default NumberButton;
--- lambda-calculator-master/src/components/ButtonComponents/NumberButtons/Numbers.js
+++ lambda-calculator-master/src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,19 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 //import any components needed
 import NumbersButton from './NumberButton';
 
 //Import your array data to from the provided data file
 
+import { numbers } from '../../../data';
+
 const Numbers = () => {
 	// STEP 2 - add the imported data to state
+	const [numbersState, setNumbersState] = useState(numbers);
+	console.log({ numbersState });
 	return (
 		<div>
 			{/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
-			Numbers Component
-			<NumbersButton />
+			{numbersState.map(number => (
+				<NumbersButton number={number} />
+			))}
 		</div>
 	);
 };
