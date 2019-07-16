64576bf0ce30f15a989ce9b7bbed8781ff69f0f0
Pat Palmerston <patpalmerston@gmail.com>
2019-07-16 10:51:47 -0700

    all buttons displayed

--- lambda-calculator-master/src/components/ButtonComponents/NumberButtons/Numbers.js
+++ lambda-calculator-master/src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -16,8 +16,8 @@ const Numbers = () => {
 			{/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
-			{numbersState.map(number => (
-				<NumbersButton number={number} />
+			{numbersState.map((number, index) => (
+				<NumbersButton number={number} key={index} />
 			))}
 		</div>
 	);
